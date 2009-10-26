d3e9be2f5aaa50929f4c4c23fdf9deff57f1b688
Bryan J Swift <bryan.j.swift@gmail.com>
2009-10-26 01:03:07 -0400

    Add information to publish jar

--- project/build/SbtPlugins.scala
+++ project/build/SbtPlugins.scala
@@ -1,3 +1,12 @@
 import sbt._
 
-class SbtPluginProject(info:ProjectInfo) extends PluginProject(info)
+class SbtPluginProject(info:ProjectInfo) extends PluginProject(info) {
+	override def managedStyle = ManagedStyle.Ivy
+	val publishTo = Resolver.sftp("Bryan J Swift","repos.bryanjswift.com",3748,"ivy2/")
+	Credentials(Path.userHome / ".ivy2" / ".credentials", log)
+	override def packageDocsJar = defaultJarPath("-javadoc.jar")
+	override def packageSrcJar= defaultJarPath("-sources.jar")
+	val sourceArtifact = Artifact(artifactID, "src", "jar", Some("sources"), Nil, None)
+	val docsArtifact = Artifact(artifactID, "docs", "jar", Some("javadoc"), Nil, None)
+	override def packageToPublishActions = super.packageToPublishActions ++ Seq(packageDocs, packageSrc)
+}
