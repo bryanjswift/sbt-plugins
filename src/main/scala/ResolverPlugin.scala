dec5800c87843be0279c17801446a5a3d13c09fc
Bryan J Swift <bryan.j.swift@gmail.com>
2010-03-29 09:41:07 -0400

    Add dummy defaults to ResolverPlugin
    This is so properties can be assigned without adding a resolver file.

--- src/main/scala/ResolverPlugin.scala
+++ src/main/scala/ResolverPlugin.scala
@@ -1,17 +1,41 @@
 package bjs.project
 
+import java.io.IOException
 import sbt._
 
 trait ResolverPlugin extends BasicManagedProject {
 	val resolverPath = propertyOptional[String](".resolver")
+	val defaultResolverName = "Example Resolver"
+	val defaultResolverHost = "example.com"
+	val defaultResolverPort = "22"
+	val defaultResolverPath = "/path/to/publish/into/"
+	val defaultResolverType = "sftp"
 	private def sbtPublishResolver = {
 		import java.io.FileInputStream
 		import java.util.Properties
-		val props = new Properties() { def apply(key:String) = this.getProperty(key) }
-		props.load(new FileInputStream(resolverPath.value))
-		props("resolver.type") match {
-			case "sftp" => Resolver.sftp(props("resolver.name"),props("resolver.host"),props("resolver.port").toInt,props("resolver.path"))
-			case "ssh" => Resolver.ssh(props("resolver.name"),props("resolver.host"),props("resolver.port").toInt,props("resolver.path"))
+		val props = new Properties() {
+			def apply(key:String) = this.getProperty(key)
+			def apply(key:String,default:String) = this.getProperty(key,default)
+		}
+		try {
+			props.load(new FileInputStream(resolverPath.value))
+		} catch {
+			case ioe:IOException => log.warn(String.format("Resolver configuration could not be found at '%s'",resolverPath.value))
+			case iae:IllegalArgumentException => log.warn(String.format("Resolver configuration could not be found at '%s'",resolverPath.value))
+		}
+		props("resolver.type",defaultResolverType) match {
+			case "sftp" =>
+				Resolver.sftp(
+					props("resolver.name",defaultResolverName),
+					props("resolver.host",defaultResolverHost),
+					props("resolver.port",defaultResolverPort).toInt,
+					props("resolver.path",defaultResolverPath))
+			case "ssh" =>
+				Resolver.ssh(
+					props("resolver.name",defaultResolverName),
+					props("resolver.host",defaultResolverHost),
+					props("resolver.port",defaultResolverPort).toInt,
+					props("resolver.path",defaultResolverPath))
 		}
 	}
 	val publishTo = sbtPublishResolver
